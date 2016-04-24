a5f62ced1e58f0e324f728934234d6783beb826e
Brenton Lin <notnerb.94@gmail.com>
2016-04-23 20:21:54 -0400

    //Added Appliance wattage selection and power switch for lights, used in conjunction with a Energy usage tracking scipt

---
+++ Assets/ApplianceSelector.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+namespace VRStandardAssets.Utils
+{
+    public class ApplianceSelector : MonoBehaviour
+    {
+        public EnergyTrackingScript other;
+        [SerializeField] private VRInteractiveItem m_InteractiveItem;
+        public int type; //wattage for the appliance;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+        private void OnEnable()
+        {
+            m_InteractiveItem.OnClick += HandleClick;
+        }
+        private void HandleClick()
+        {
+            //Set the appliance to it's type's wattage
+            SetAppliance(type);
+        }
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public void SetAppliance(int select)
+        {
+            //continue adding types for other appliance constants
+            if (select == 1)
+            {
+                other.bulbconstant = 60; //incandescent
+             
+            }
+            if (select == 2)
+            {
+                other.bulbconstant = 15; //CFL
+                
+            }
+            if (select == 3)
+            {
+                other.bulbconstant = 12; //LED
+                
+            }
+        }
+    }
+}
---
+++ Assets/EnergyTrackingScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyTrackingScript : MonoBehaviour {
+    public double time;
+    public double kWh;
+    public double jouUsed; //joules of energy consumed;
+    public double bulbs = 0;   //how many bulbs are on
+    public double bulbconstant; //wattage rating of the installed bulbs
+    public int tv = 0; //is the tv on?
+    public int dryer = 0; //is the dryer on?
+    public int washer = 0; //is the washer on?
+    public bool run; //is the simulation on or stopped?
+    
+    // Use this for initialization
+	void Start () {
+        InvokeRepeating("UpdateEnergy",0,1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void UpdateEnergy()
+    {
+        if (run)
+        {
+            time += 1; //timer from beginning of sim;
+            jouUsed += bulbs * bulbconstant;
+            jouUsed += 213 * tv; //wattage of a powered TV.
+        }
+    }
+    double kWhcalc()
+    {
+        kWh = (jouUsed / time) * 3.6; //kWh calculation for the course of the simulation;
+        return kWh;
+    }
+}

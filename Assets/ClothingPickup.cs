0548470b95e541583b06ea40a19dc0eb4375141b
Brett Belliveau <brettbelliveau@outlook.com>
2016-04-26 10:34:57 -0400

    Started clothing activity models/scripts

---
+++ Assets/ClothingPickup.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace VRStandardAssets.Utils
+{
+    public class ClothingPickup : MonoBehaviour
+    {
+        
+        public EnergyTrackingScript other; //drag the main EnergyTracking script here;
+        [SerializeField]private VRInteractiveItem m_InteractiveItem;
+
+        public int objects; //how many objects does this switch control?
+        public GameObject[] LightArr = new GameObject[10]; //the lights this switch controls in the scene;
+        
+        // Use this for initialization
+        bool on = false;
+        void Start()
+        {
+
+        }
+        private void OnEnable()
+        {
+            m_InteractiveItem.OnClick += HandleClick;
+        }
+        private void HandleClick()
+        {
+            //turn on lights at a click;
+            SetLights();
+        }
+        public void SetLights() //turn lights on (adding them to the wattage calculation.)
+        {
+            if (on)
+            {
+                on = false;
+                other.bulbs -= objects;
+            }
+            else {
+                on = true;
+                other.bulbs += objects;
+            }
+            //write function to active lighting component of the GameObjects in the light array.
+        }
+    }
+}
